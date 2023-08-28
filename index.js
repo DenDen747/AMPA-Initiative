196163b2af751aa774e8037e8ba02fff4ac6306f
Denes Garda <dsgarda@gmail.com>
2023-08-27 22:22:00 -0700

    publish

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
+
+app.use('/favicon.ico', express.static('public/icon.ico'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "home", 'home.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/our-team', (req, res) => {
+  res.sendFile(path.join(__dirname, 'our-team.html'));
+})
 
 app.listen(process.env.PORT || 85, () => {
   console.log('Webserver started.');
